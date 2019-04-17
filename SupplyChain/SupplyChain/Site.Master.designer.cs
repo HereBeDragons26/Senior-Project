23810d8e09bf2f0551930c3f851b232c5af626b8
Berk Günseli <berkgunseli@gmail.com>
2019-04-17 17:59:57 +0300

    Login Fix

--- SupplyChain/SupplyChain/Site.Master.designer.cs
+++ SupplyChain/SupplyChain/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace SupplyChain {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// LoginButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button LoginButton;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
@@ -47,5 +38,14 @@ namespace SupplyChain {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
     }
 }
