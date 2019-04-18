0db7821e120fda28ed49ba422c0f119ba52f9ab1
Berk Günseli <berkgunseli@gmail.com>
2019-04-18 23:01:38 +0300

    Login Fix

--- SupplyChain/SupplyChain/Site.Master.designer.cs
+++ SupplyChain/SupplyChain/Site.Master.designer.cs
@@ -47,5 +47,14 @@ namespace SupplyChain {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
     }
 }
