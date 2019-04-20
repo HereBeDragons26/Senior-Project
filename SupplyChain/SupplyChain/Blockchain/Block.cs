fcfe9231f8a0e8635406ea69ef8cefeb4259fd85
rmznsn <32266249+rmznsn@users.noreply.github.com>
2019-04-20 14:31:44 +0300

    Blockchain

--- SupplyChain/SupplyChain/Blockchain/Block.cs
+++ SupplyChain/SupplyChain/Blockchain/Block.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace SupplyChain.Blockchain {
     public class Block {
-        private DateTime islemTarihi { get; set; }
-        private String aciklama { get; set; }
+        private DateTime Times { get; set; }
+        private int ProductID { get; set; }
+        private int CompanyID { get; set; }
+        private string Data { get; set; }
+        private string Hash { get; set; }
+        private string PreviousHash { get; set; }
+        private int Index { get; set; }
+
+
+        public Block(DateTime times,string data,string previoushash,int index,int companyID,int productID)
+        {
+            Times = times;
+            Data = data;
+            Hash = calculateHash();
+            PreviousHash = previoushash;
+            Index = index;
+            CompanyID = companyID;
+            ProductID = productID;
+        }
+
+        public string calculateHash()
+        {
+            SHA256 sHA256 = SHA256.Create();
+            byte[] input = Encoding.ASCII.GetBytes(Data + ProductID.ToString() + CompanyID.ToString());
+            byte[] output = sHA256.ComputeHash(input);
+
+            return Convert.ToString(output);
+
+
+        }
         
         
 
---
+++ SupplyChain/SupplyChain/Blockchain/Blockchain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupplyChain.Blockchain
+{
+    public class Blockchain
+    {
+         public IList<Block> chain { get; set; }
+
+        public Blockchain()
+        {
+            InitializeChain();
+        }
+        public void InitializeChain()
+        {
+            chain = new List<Block>();
+        }
+
+        public Block CreateBlock(string data,string previoushash,int index,int productID,int companyID)
+        {
+            return new Block(DateTime.Now, data, previoushash, index, productID, companyID);
+        } 
+        
+    }
+}
