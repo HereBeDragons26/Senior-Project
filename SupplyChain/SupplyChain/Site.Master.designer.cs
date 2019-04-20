7d948b40da3016192d651d9f96bfc762b4099358
eabx2 <enabx2@protonmail.com>
2019-04-20 13:28:50 +0300

    SqlDataSource

--- SupplyChain/SupplyChain/Site.Master.designer.cs
+++ SupplyChain/SupplyChain/Site.Master.designer.cs
@@ -83,5 +83,14 @@ namespace SupplyChain {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button LoginButton;
+        
+        /// <summary>
+        /// UsersDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource UsersDataSource;
     }
 }
